1d4427db4febaee77f878354303f8e96f3115052
sofia <sofia@faem.me>
2021-09-21 14:06:43 +0300

    task1, constructor pizza page (data output from json)

---
+++ src/frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="content">
+    <Index></Index>
+  </div>
+</template>
+
+<script>
+import Index from "@/components/Index";
+export default {
+  name: "AppLayout",
+  components: {
+    Index,
+  },
+};
+</script>
