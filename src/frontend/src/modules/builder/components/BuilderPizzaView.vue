2bd125e82c52d969cce80e1021da476c5e3a6863
sofia <medi61200@mail.ru>
2021-10-21 01:38:58 +0300

    drag in drop

--- src/frontend/src/modules/builder/components/BuilderPizzaView.vue
+++ src/frontend/src/modules/builder/components/BuilderPizzaView.vue
@@ -1,16 +1,27 @@
 <template>
-  <div class="content__constructor">
-    <div class="pizza pizza--foundation--big-tomato">
-      <div class="pizza__wrapper">
-        <div class="pizza__filling pizza__filling--ananas"></div>
-        <div class="pizza__filling pizza__filling--bacon"></div>
-        <div class="pizza__filling pizza__filling--cheddar"></div>
+  <app-drop @drop="addIngredient">
+    <div class="content__constructor">
+      <div class="pizza pizza--foundation--big-tomato">
+        <div class="pizza__wrapper">
+          <div class="pizza__filling pizza__filling--ananas"></div>
+          <div class="pizza__filling pizza__filling--bacon"></div>
+          <div class="pizza__filling pizza__filling--cheddar"></div>
+        </div>
       </div>
     </div>
-  </div>
+  </app-drop>
 </template>
 <script>
+import AppDrop from "@/common/components/AppDrop";
 export default {
   name: "BuilderPizzaView",
+  components: {
+    AppDrop,
+  },
+  methods: {
+    addIngredient(ingredient) {
+      console.log(ingredient);
+    },
+  },
 };
 </script>
